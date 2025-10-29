8e67fc020a74fbaa5d0085a571abc99e64a252bd
hugofavre <favrehugo1@gmail.com>
2025-10-29 09:16:00 +0100

    Added cartesian - orbital elements conversion in order to better simulate and visualize stability of the simulation

--- src/earth_satellite_display.py
+++ src/earth_satellite_display.py
@@ -1,25 +1,37 @@
 import matplotlib.pyplot as plt
 from earth_satellite_solve import solve
+from earth_satellite_solve import rv_to_oe
+from earth_satellite_solve import oe_to_rv
+from earth_satellite_solve import calc_period
 from readfile import read_input_file
 import sys
-#from mpl_toolkits.mplot3d import Axes3D
 
 if __name__ == "__main__":
-
     # Recovering initial conditions from the input file
     filename = sys.argv[1]
     params = read_input_file(filename)
-    
-    t = params.get("time_days")
-    x_0 = params.get("x_0")
-    y_0 = params.get("y_0")
-    z_0 = params.get("z_0")
-    vx_0 = params.get("vx_0")
-    vy_0 = params.get("vy_0")
-    vz_0 = params.get("vz_0")
+    if 'a' in params:
+        # In this case, we input orbital elements. to solve, we first convert to cartesian.
+        a = params['a']
+        r_vec, v_vec = oe_to_rv(a=a, e=params['e'], i_deg=params.get('i_deg', 0),
+        RAAN_deg=params.get('RAAN_deg', 0), argp_deg=params.get('argp_deg', 0), nu_deg=params.get('nu_deg', 0))
+        x0, y0, z0 = r_vec
+        vx0, vy0, vz0 = v_vec
+        n_orbits = params['n_orbits']
+        t = calc_period(a)*n_orbits
+        
+    else:
+        # In this case, we already input cartesian values.
+        t = params["time_days"]*24*3600
+        x0 = params["x0"]
+        y0 = params["y0"]
+        z0 = params["z0"]
+        vx0 = params["vx0"]
+        vy0 = params["vy0"]
+        vz0 = params["vz0"]
 
     # We solve the two-body differential equation and then plot the results.
-    solution = solve((0,t*24*3600), x_0, y_0, z_0, vx_0, vy_0, vz_0)    
+    solution = solve((0,t), x0, y0, z0, vx0, vy0, vz0)    
     x, y, z = solution.y[0], solution.y[1], solution.y[2]
 
     # Plotting
@@ -32,3 +44,6 @@ if __name__ == "__main__":
     ax.set_zlabel('Z (km)')
     ax.legend()
     plt.show()
+
+    
+
--- src/earth_satellite_solve.py
+++ src/earth_satellite_solve.py
@@ -1,10 +1,10 @@
 import numpy as np
 from scipy.integrate import solve_ivp
+mu = 3.986004418e14 
 
 # Function describing the two-body differential equation with m_earth >> m_satellite, 3D.
 def two_body_3D(t,coord):
     x,y,z,vx,vy,vz = coord
-    mu = 3.986004418e14 
     r = np.sqrt(x**2 + y**2 + z**2)
 
     ax = -mu*x/(r**3)
@@ -19,3 +19,92 @@ def solve(timespan, x_0, y_0, z_0, vx_0, vy_0, vz_0,):
     coord_0 = [x_0, y_0, z_0, vx_0, vy_0, vz_0]
     solution = solve_ivp(two_body_3D, timespan, coord_0, t_eval = time_eval, rtol=1e-10, atol=1e-13, method = "RK45")
     return solution
+
+# Function converting the current position and speed of the satellite to orbital elements.
+def rv_to_oe(r_vec, v_vec):
+    r = np.array(r_vec, dtype=float)
+    v = np.array(v_vec, dtype=float)
+    r_norm = np.linalg.norm(r)
+    v_norm = np.linalg.norm(v)
+
+    h = np.cross(r, v)
+    h_norm = np.linalg.norm(h)
+
+    k = np.array([0.0, 0.0, 1.0])
+    n = np.cross(k, h)
+    n_norm = np.linalg.norm(n)
+
+    energy = v_norm**2 / 2 - mu / r_norm
+
+    if abs(energy) > 0:
+        a = -mu / (2 * energy)
+    else:
+        a = np.inf
+
+    e_vec = (1/mu) * ( (v_norm**2 - mu/r_norm)*r - np.dot(r, v)*v )
+    e = np.linalg.norm(e_vec)
+
+    i = np.arccos(h[2] / h_norm)
+
+    if n_norm != 0:
+        RAAN = np.arccos(n[0] / n_norm)
+        if n[1] < 0:
+            RAAN = 2*np.pi - RAAN
+    else:
+        RAAN = 0.0
+
+    if n_norm != 0 and e > 1e-12:
+        argp = np.arccos(np.dot(n, e_vec) / (n_norm * e))
+        if e_vec[2] < 0:
+            argp = 2*np.pi - argp
+    else:
+        argp = 0.0
+
+    if e > 1e-12:
+        nu = np.arccos(np.dot(e_vec, r) / (e * r_norm))
+        if np.dot(r, v) < 0:
+            nu = 2*np.pi - nu
+    else:
+        if n_norm != 0:
+            nu = np.arccos(np.dot(n, r) / (n_norm * r_norm))
+            if r[2] < 0:
+                nu = 2*np.pi - nu
+        else:
+            nu = 0.0
+
+    # Convert to conventional units
+    return [a,e,np.degrees(i), np.degrees(RAAN), np.degrees(argp), np.degrees(nu), h, energy]
+
+def oe_to_rv(a, e, i_deg, RAAN_deg, argp_deg, nu_deg):
+    i = np.radians(i_deg)
+    RAAN = np.radians(RAAN_deg)
+    argp = np.radians(argp_deg)
+    nu = np.radians(nu_deg)
+
+    p = a * (1 - e**2)
+    r = p / (1 + e*np.cos(nu))
+    h = np.sqrt(mu * p)
+
+    # position et vitesse dans le plan orbital
+    r_orb = np.array([r*np.cos(nu), r*np.sin(nu), 0.0])
+    v_orb = np.array([-mu/h * np.sin(nu), mu/h * (e + np.cos(nu)), 0.0])
+
+    # matrices de rotation
+    R3_W = np.array([[ np.cos(RAAN), -np.sin(RAAN), 0],
+                     [ np.sin(RAAN),  np.cos(RAAN), 0],
+                     [ 0, 0, 1]])
+    R1_i = np.array([[1, 0, 0],
+                     [0, np.cos(i), -np.sin(i)],
+                     [0, np.sin(i),  np.cos(i)]])
+    R3_w = np.array([[ np.cos(argp), -np.sin(argp), 0],
+                     [ np.sin(argp),  np.cos(argp), 0],
+                     [ 0, 0, 1]])
+
+    R = R3_W @ R1_i @ R3_w
+    r_ECI = R @ r_orb
+    v_ECI = R @ v_orb
+    return r_ECI, v_ECI
+
+
+def calc_period(a):
+    return 2*np.pi*np.sqrt(a**3/mu)
