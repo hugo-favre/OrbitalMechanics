c4e60692b1f776a365539d1d010b1938f7fb901c
hugofavre <favrehugo1@gmail.com>
2025-10-31 15:38:59 +0100

    Added main.py to centralize all display and analysis functions. Created blueprint for perturbations

---
+++ main.py
@@ -0,0 +1,21 @@
+from src.display.earth_satellite_display import orbit_display
+from src.display.two_body_display import twobody_display
+from src.analysis.orbit_analysis import orbit_analysis
+from src.tools.readfile import read_input_file
+
+def main():
+    filename = "data/inputs/two_body_3.txt"
+    params = read_input_file(filename)
+
+    mode = params['mode']
+    perturbations = params['perturbations'] # Useless for now
+    for m in mode:
+        if m == 'orbit_display':
+            orbit_display(params)
+        if m == 'orbit_analysis':
+            orbit_analysis(params)
+        if m == 'two_body_display':
+            twobody_display(params)
+
+if __name__ == "__main__":
+    main()
